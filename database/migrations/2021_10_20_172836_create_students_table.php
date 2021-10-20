f327f71151ece2a3ddee459d8961778f9164d152
JonathanDieke <louloulexus@gmail.com>
2021-10-20 17:58:56 +0000

    init projet, création des models/migrations/factories

---
+++ database/migrations/2021_10_20_172836_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('register')->unique();
+            $table->string('name');
+            $table->string('lname');
+            $table->date('birthday');
+            $table->string('gender');
+            $table->foreignId('classroom_id')->constrained('classrooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/migrations/2021_10_20_173112_create_classrooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('libel');
+            $table->foreignId('level_id')->constrained('levels');
+            $table->foreignId('option_id')->constrained('options');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classrooms');
+    }
+}
